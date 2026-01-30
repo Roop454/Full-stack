22e82849cbfa6abaccbb5dba57da2fcd4d214731
Roop454 <roop.dhiman454@gmail.com>
2026-01-30 12:47:33 +0530

    delete

--- Experiment 3/exp3_a/src/About.js
+++
@@ -1,9 +0,0 @@
-function About() {
-  return (
-    <div className="page">
-      <h2>ℹ️ About</h2>
-      <p>This experiment demonstrates SPA routing.</p>
-    </div>
-  );
-}
-export default About;
--- Experiment 3/exp3_a/src/Home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="page">
-      <h2>🏠 Home</h2>
-      <p>Welcome to the React Routing Experiment.</p>
-    </div>
-  );
-}
-export default Home;
